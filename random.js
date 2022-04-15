01a51b8e5c3c99fc13caa6a35c6d4af22b496fd4
jchooch <joechoochoy@gmail.com>
2022-04-15 13:43:25 -0400

    could it be countenance

--- random.js
+++ random.js
@@ -6,14 +6,14 @@ var random_text = new random_text();
 // Set the number of text strings to zero to start
 var number = 0;
 // Incremental list of all possible Text
-random_text[number++] = "<img src='{{site.photo_1}}' class='img-fluid rounded float-left' alt='business'>"
-random_text[number++] = "<img src='{{site.photo_2}}' class='img-fluid rounded float-left' alt='monsieur'>"
-random_text[number++] = "<img src='{{site.photo_3}}' class='img-fluid rounded float-left' alt='nyc'>"
-random_text[number++] = "<img src='{{site.photo_4}}' class='img-fluid rounded float-left' alt='blue'>"
-random_text[number++] = "<img src='{{site.photo_5}}' class='img-fluid rounded float-left' alt='joshua tree'>"
-random_text[number++] = "<img src='{{site.photo_6}}' class='img-fluid rounded float-left' alt='tent'>"
-random_text[number++] = "<img src='{{site.photo_7}}' class='img-fluid rounded float-left' alt='pandemic couture'>"
-random_text[number++] = "<img src='{{site.photo_8}}' class='img-fluid rounded float-left' alt='asheville'>"
+random_text[number++] = "<img src='{{site.photo_1}}' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='{{site.photo_2}}' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='{{site.photo_3}}' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='{{site.photo_4}}' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='{{site.photo_5}}' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='{{site.photo_6}}' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='{{site.photo_7}}' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='{{site.photo_8}}' class='img-fluid rounded float-left' alt='countenance'>"
 // Create a random number with limits based on the number
 // of possible random text strings
 function getRandom(){

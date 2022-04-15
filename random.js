ac9c789f654fb1467113d0203eaa85df14940d7e
jchooch <joechoochoy@gmail.com>
2022-04-15 14:42:55 -0400

    crude simple probs

--- random.js
+++ random.js
@@ -5,8 +5,12 @@ function random_text()
 var random_text = new random_text();
 var number = 0;
 random_text[number++] = "<img src='/assets/photo_1.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_1.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_2.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
 random_text[number++] = "<img src='/assets/photo_2.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
 random_text[number++] = "<img src='/assets/photo_3.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_3.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_4.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
 random_text[number++] = "<img src='/assets/photo_4.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
 random_text[number++] = "<img src='/assets/photo_5.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
 random_text[number++] = "<img src='/assets/photo_6.jpeg' class='img-fluid rounded float-left' alt='countenance'>"

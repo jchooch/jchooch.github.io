44e3bb0ba05e1ab57e6c14b8431f8d0faca1b810
jchooch <joechoochoy@gmail.com>
2022-04-15 14:04:30 -0400

    really trying here

--- random.js
+++ random.js
@@ -16,18 +16,15 @@ random_text[number++] = "<img src='{{site.photo_7}}' class='img-fluid rounded fl
 random_text[number++] = "<img src='{{site.photo_8}}' class='img-fluid rounded float-left' alt='countenance'>";
 // Create a random number with limits based on the number
 // of possible random text strings
-function getRandom(){
-  var num = Math.random();
-  if(num < 0.175) return 1;
-  else if(num < 0.350) return 2;
-  else if(num < 0.500) return 3;
-  else if(num < 0.650) return 4;
-  else if(num < 0.725) return 5;
-  else if(num < 0.825) return 6;
-  else if(num < 0.900) return 7;
-  else return 8;
-}
-var rand_num = getRandom();
+var num = Math.random();
+if(num < 0.175) var rand_num = 1;
+else if(num < 0.350) var rand_num = 2;
+else if(num < 0.500) var rand_num = 3;
+else if(num < 0.650) var rand_num = 4;
+else if(num < 0.725) var rand_num = 5;
+else if(num < 0.825) var rand_num = 6;
+else if(num < 0.900) var rand_num = 7;
+else var rand_num = 8;
 var random_number = Math.floor(Math.random() * number);
 // Write out the random text to the browser
 document.write(random_text[rand_num]);

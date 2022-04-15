28bc61df9d80dcbf7c1c072f2bcde95a17142afc
jchooch <joechoochoy@gmail.com>
2022-04-15 16:57:32 -0400

    std size

--- random.js
+++ random.js
@@ -4,17 +4,17 @@ function random_text()
 {};
 var random_text = new random_text();
 var number = 0;
-random_text[number++] = "<img src='/assets/photo_1.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
-random_text[number++] = "<img src='/assets/photo_1.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
-random_text[number++] = "<img src='/assets/photo_2.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
-random_text[number++] = "<img src='/assets/photo_2.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
-random_text[number++] = "<img src='/assets/photo_3.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
-random_text[number++] = "<img src='/assets/photo_3.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
-random_text[number++] = "<img src='/assets/photo_4.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
-random_text[number++] = "<img src='/assets/photo_4.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
-random_text[number++] = "<img src='/assets/photo_5.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
-random_text[number++] = "<img src='/assets/photo_6.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
-random_text[number++] = "<img src='/assets/photo_7.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
-random_text[number++] = "<img src='/assets/photo_8.jpeg' class='img-fluid rounded float-left' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_1.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_1.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_2.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_2.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_3.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_3.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_4.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_4.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_5.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_6.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_7.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
+random_text[number++] = "<img src='/assets/photo_8.jpeg' class='img-fluid rounded float-left' width='250' height='350' alt='countenance'>"
 var random_number = Math.floor(Math.random() * number);
 document.write(random_text[random_number]);

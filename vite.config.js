5ba490b5d88dd159a5b15b10bd0ae0a0e5af75b8
jchooch <joechoochoy@gmail.com>
2023-09-17 15:38:22 +0100

    new build with altered base path in attempt to fix mime error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/jchooch.github.io",
+  base: "./jchooch.github.io",
   plugins: [react()],
 })
